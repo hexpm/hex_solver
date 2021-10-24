c007a6cdec786f425acad8b6dd315090fc0cdf8b
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2021-10-24 17:59:20 +0200

    Add CI (#1)

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Resolver.MixProject do
     [
       app: :resolver,
       version: "0.1.0",
-      elixir: "~> 1.13-dev",
+      elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
