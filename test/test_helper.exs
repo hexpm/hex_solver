4cd5891e629d6687dfc89eddc246df24d5b14008
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2021-10-26 21:12:29 +0200

    Move process registry to test helper

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -66,4 +66,36 @@ defmodule Resolver.Case do
   end
 end
 
+defmodule Resolver.Registry.Process do
+  @behaviour Resolver.Registry
+
+  def versions(package) do
+    case Process.get({__MODULE__, :versions, package}) do
+      nil -> :error
+      versions -> {:ok, versions}
+    end
+  end
+
+  def dependencies(package, version) do
+    case Process.get({__MODULE__, :dependencies, package, version}) do
+      nil -> :error
+      dependencies -> {:ok, dependencies}
+    end
+  end
+
+  def put(package, version, dependencies) do
+    version = Version.parse!(version)
+    versions = Process.get({__MODULE__, :versions, package}, [])
+    versions = Enum.sort([version | versions], Version)
+
+    dependencies =
+      Enum.map(dependencies, fn {package, requirement} ->
+        {package, {Resolver.Requirement.to_constraint!(requirement)}}
+      end)
+
+    Process.put({__MODULE__, :versions, package}, versions)
+    Process.put({__MODULE__, :dependencies, package, version}, dependencies)
+  end
+end
+
 Resolver.Case.init_registry()
